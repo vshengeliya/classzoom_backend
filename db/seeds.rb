a721ad074be4693ba02775a37367d6da1d5d6385
kylejb <kjburda@gmail.com>
2020-08-17 18:02:18 -0400

    Updated seed.rb file with sample data that will be later replaced with API data on call

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@ Teacher.destroy_all
 puts "#2 - Seeding database with..."
 
 flatiron = Classroom.create!( name: "The Best Cohort: 06-22-20" )
-teacher_obj = Teacher.create!( name: "Test-TeacherName", email: "Test-TeacherEmail", zoom_url: "http://placeholder.for.zoom.url.com")
+teacher_obj = Teacher.create!( name: "Test-TeacherName", email: "Test-TeacherEmail@invalid.com", current_zoom_meeting_url: "https://us04web.zoom.us/j/1234567890?pwd=R3ZBU2JBN3NnOTlDbHh1VjR4Q1NhUT09", current_zoom_meeting_id: "1234567890", current_zoom_meeting_password: "password", current_zoom_meeting_start_time: "08-20-2020 20:00", current_zoom_meeting_duration: 60)
 
 puts "#2a - ... students, teacher:"
 (1..15).each do |i|
-    student_obj = Student.create!( name: "Test-StudentName#{i}", email: "Test-StudentEmail#{i}", zoom_url: "http://placeholder.for.zoom.url.com")
+    student_obj = Student.create!( name: "Test-StudentName#{i}", email: "Test-StudentEmail#{i}@invalid.com", current_zoom_meeting_url: "https://us04web.zoom.us/j/1234567890?pwd=R3ZBU2JBN3NnOTlDbHh1VjR4Q1NhUT09", current_zoom_meeting_id: "1234567890", current_zoom_meeting_password: "password", current_zoom_meeting_start_time: "08-20-2020 20:00", current_zoom_meeting_duration: 60)
 end
 
 
@@ -26,7 +26,7 @@ end
 puts "#2b - ... events to one classroom"
 
 (1..5).each do |i|
-    Event.create!( name: "Test-Event#{i}", date: Time.now.strftime("%F"), time: Time.now.strftime("%H:%M"), description: "Test-Desc#{i}" , zoom_url: "http://placeholder.for.zoom.url.com", classroom: flatiron )
+    Event.create!( name: "Test-Event#{i}", date: Time.now.strftime("%F"), time: Time.now.strftime("%H:%M"), description: "Test-Desc#{i}" , zoom_url: "https://us04web.zoom.us/j/1234567890?pwd=R3ZBU2JBN3NnOTlDbHh1VjR4Q1NhUT09", classroom: flatiron )
 end
 
 puts "Seeding is complete"
