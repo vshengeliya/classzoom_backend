2b0cc78b1211e03fce15542d1adfe02894354ded
kylejb <kjburda@gmail.com>
2020-08-17 15:04:31 -0400

    Refactoring 'zoom_jwt' by splitting its response method to a new model ('zoom_runner.rb') for Zoom API handling

--- app/models/zoom_jwt.rb
+++ app/models/zoom_jwt.rb
@@ -1,5 +1,3 @@
-require 'uri'
-require 'net/http'
 require 'jwt'
 
 module ZoomJwt
@@ -16,22 +14,6 @@ module ZoomJwt
                 "appKey": API_KEY
             }
             return JWT.encode(payload, SECRET_KEY, "HS256", {typ: "JWT"})
-        end
-
-        def self.response
-            url = URI("https://api.zoom.us/v2/users/me")
-            
-            http = Net::HTTP.new(url.host, url.port)
-            http.use_ssl = true
-            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-            request = Net::HTTP::Get.new(url)
-            request["authorization"] = "Bearer #{self.make_jwt}"
-            request["content-type"] = "application/json"
-
-            response = http.request(request)
-            puts "response.read_body"
-            puts response.read_body
         end
     end
 end
