d6f95023243c148aba1349e21a62c93fc8a006d8
kylejb <kjburda@gmail.com>
2020-08-18 13:34:05 -0400

    Revised datatypes for meeting_start_time, added is_at_desk? for students and teachers, refactored models' helper methods

--- db/migrate/20200817212020_add_zoom_meeting_attributes_to_students.rb
+++ db/migrate/20200817212020_add_zoom_meeting_attributes_to_students.rb
@@ -1,11 +1,13 @@
 class AddZoomMeetingAttributesToStudents < ActiveRecord::Migration[6.0]
   def change
-    remove_column :students, :zoom_url
+    remove_column :students, :zoom_url, :string
     
     add_column :students, :current_zoom_meeting_url, :string
     add_column :students, :current_zoom_meeting_id, :string
     add_column :students, :current_zoom_meeting_password, :string
-    add_column :students, :current_zoom_meeting_start_time, :string
+    add_column :students, :current_zoom_meeting_start_time, :datetime
     add_column :students, :current_zoom_meeting_duration, :integer
+
+    add_column :students, :is_at_desk?, :boolean, null: false, default: true
   end
 end
