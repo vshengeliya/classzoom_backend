ad0bf29985a9425cda710a696e2af8edecd3f3a7
kylejb <kjburda@gmail.com>
2020-08-19 19:47:47 -0400

    ZoomAPI - wired up meeting creation for students via controller

--- app/controllers/zoom_meetings_controller.rb
+++ app/controllers/zoom_meetings_controller.rb
@@ -5,7 +5,9 @@ class ZoomMeetingsController < ApplicationController
         new_meeting = ZoomRunner.create_meeting        
         @current_user.update(current_zoom_meeting_url: new_meeting["join_url"], current_zoom_meeting_id: new_meeting["id"], current_zoom_meeting_password: new_meeting["password"], current_zoom_meeting_start_time: new_meeting["start_time"].to_datetime, , current_zoom_meetin
             _duration: new_meeting["duration"] )
-
+        
+        # revisit how we'd like to render response after exploring some stretch features/goals
+        render json: {status: 'meeting is ready'}
     end
     
     private
@@ -13,6 +15,6 @@ class ZoomMeetingsController < ApplicationController
     def find_current_user
         #! confirm how params are structured when passed from frontend
         #? also, incorporate filtering by Model through route?
-        @current_user = Student.find(params[:id])
+        @current_user = Student.find(params[:student][:id])
     end
 end
