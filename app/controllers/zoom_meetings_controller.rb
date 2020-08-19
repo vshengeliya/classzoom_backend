acd05e2a40e17451ef404ec67580e2653e6c9ce3
kylejb <kjburda@gmail.com>
2020-08-19 16:14:49 -0400

    POST to Zoom API for new meetings and updates Student model

--- app/controllers/zoom_meetings_controller.rb
+++ app/controllers/zoom_meetings_controller.rb
@@ -1,7 +1,18 @@
 class ZoomMeetingsController < ApplicationController
+    before_action :find_current_user, only: [:create]
 
     def create
-        
-    end
+        new_meeting = ZoomRunner.create_meeting        
+        @current_user.update(current_zoom_meeting_url: new_meeting["join_url"], current_zoom_meeting_id: new_meeting["id"], current_zoom_meeting_password: new_meeting["password"], current_zoom_meeting_start_time: new_meeting["start_time"].to_datetime, , current_zoom_meetin
+            _duration: new_meeting["duration"] )
 
+    end
+    
+    private
+
+    def find_current_user
+        #! confirm how params are structured when passed from frontend
+        #? also, incorporate filtering by Model through route?
+        @current_user = Student.find(params[:id])
+    end
 end
--- app/models/zoom_runner.rb
+++ app/models/zoom_runner.rb
@@ -2,11 +2,11 @@ require 'uri'
 require 'net/http'
 
 module ZoomRunner
-    ZOOM_API_VERSION = "v2"
-    BASE_URI = "https://api.zoom.us/#{ZOOM_API_VERSION}/users"
-    #PREFIX_URI = "/users"
-    TARGET_USER_EP = "me"
-    MEETING_EP = "/meetings"
+    ZOOM_API_VERSION = 'v2'
+    BASE_URI = 'https://api.zoom.us/#{ZOOM_API_VERSION}/users'
+    #PREFIX_URI = '/users'
+    TARGET_USER_EP = 'me'
+    MEETING_EP = '/meetings'
 
     # #TODO - enable dynamic behavior for various endpoints based on frontend user actions... let's narrow down to MVP scope of meeting actions
     # def self.url_builder
@@ -17,6 +17,11 @@ module ZoomRunner
 # To request a User’s ZAK, send a GET request with a userId to /users/{userId]/token and specify type=zak in the body of the request. (If a type is not specified, the default response will be Zoom Token). .
 
 # A User ID can either be a userID requested through the  or the user’s email address.
+    def self.schedule_for_time
+        arr = DateTime.now.end_of_minute.strftime.split('-')
+        arr.pop
+        return arr.join('-')
+    end
 
     def self.create_meeting
         url = URI("https://api.zoom.us/v2/users/#{TARGET_USER_EP}/meetings")
@@ -26,30 +31,93 @@ module ZoomRunner
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
         request = Net::HTTP::Post.new(url)
-        request["content-type"] = 'application/json'
-        request["authorization"] = "Bearer #{ZoomJwt::Auth.make_jwt}"
-        request.body = "{\"topic\":\"string\",\"type\":\"integer\",\"start_time\":\"string [date-time]\",\"duration\":\"integer\",\"schedule_for\":\"string\",\"timezone\":\"string\",\"password\":\"string\",\"agenda\":\"string\",\"recurrence\":{\"type\":\"integer\",\"repeat_interval\":\"integer\",\"weekly_days\":\"string\",\"monthly_day\":\"integer\",\"monthly_week\":\"integer\",\"monthly_week_day\":\"integer\",\"end_times\":\"integer\",\"end_date_time\":\"string [date-time]\"},\"settings\":{\"host_video\":\"boolean\",\"participant_video\":\"boolean\",\"cn_meeting\":\"boolean\",\"in_meeting\":\"boolean\",\"join_before_host\":\"boolean\",\"mute_upon_entry\":\"boolean\",\"watermark\":\"boolean\",\"use_pmi\":\"boolean\",\"approval_type\":\"integer\",\"registration_type\":\"integer\",\"audio\":\"string\",\"auto_recording\":\"string\",\"enforce_login\":\"boolean\",\"enforce_login_domains\":\"string\",\"alternative_hosts\":\"string\",\"global_dial_in_countries\":[\"string\"],\"registrants_email_notification\":\"boolean\"}}"
+        request['content-type'] = 'application/json'
+        request['authorization'] = "Bearer #{ZoomJwt::Auth.make_jwt}"
+        zoom_body = {
+            'topic': 'string',
+            'type': 2,
+            'start_time': self.schedule_for_time,
+            'duration': 60,
+            'password': 'Flatiron20',
+            'settings': {
+              'host_video': false,
+              'participant_video': false,
+              'cn_meeting': false,
+              'in_meeting': false,
+              'join_before_host': true,
+              'mute_upon_entry': false,
+              'use_pmi': false,
+              'approval_type': 2,
+              'audio': 'both',
+              'auto_recording': 'local',
+              'enforce_login': false
+            }
+        }
+
+        # zoom_body = {
+        #     'topic': 'string',
+        #     'type': 'integer',
+        #     'start_time': 'string [date-time]',
+        #     'duration': 'integer',
+        #     'schedule_for': 'string',
+        #     'timezone': 'string',
+        #     'password': 'string',
+        #     'agenda': 'string',
+        #     'recurrence': {
+        #       'type': 'integer',
+        #       'repeat_interval': 'integer',
+        #       'weekly_days': 'string',
+        #       'monthly_day': 'integer',
+        #       'monthly_week': 'integer',
+        #       'monthly_week_day': 'integer',
+        #       'end_times': 'integer',
+        #       'end_date_time': 'string [date-time]'
+        #     },
+        #     'settings': {
+        #       'host_video': 'boolean',
+        #       'participant_video': 'boolean',
+        #       'cn_meeting': 'boolean',
+        #       'in_meeting': 'boolean',
+        #       'join_before_host': 'boolean',
+        #       'mute_upon_entry': 'boolean',
+        #       'watermark': 'boolean',
+        #       'use_pmi': 'boolean',
+        #       'approval_type': 'integer',
+        #       'registration_type': 'integer',
+        #       'audio': 'string',
+        #       'auto_recording': 'string',
+        #       'enforce_login': 'boolean',
+        #       'enforce_login_domains': 'string',
+        #       'alternative_hosts': 'string',
+        #       'global_dial_in_countries': [
+        #         'string'
+        #       ],
+        #       'registrants_email_notification': 'boolean'
+        #     }
+        #   }
+        request.body = zoom_body.to_json
+        
 
         response = http.request(request)
-        puts response.read_body
-
+        
+        return JSON.parse(response.read_body)
     end
 
 
 
     def self.response
-        url = URI("https://api.zoom.us/v2/users/me/token?type=zak")
+        url = URI('https://api.zoom.us/v2/users/me/token?type=zak')
 
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
         request = Net::HTTP::Get.new(url)
-        request["authorization"] = "Bearer #{ZoomJwt::Auth.make_jwt}"
-        request["content-type"] = "application/json"
+        request['authorization'] = 'Bearer #{ZoomJwt::Auth.make_jwt}'
+        request['content-type'] = 'application/json'
 
         response = http.request(request)
-        puts "response.read_body"
+        puts 'response.read_body'
         puts response.read_body
     end
 
