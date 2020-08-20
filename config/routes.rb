85a2b60bf56bb519e3358588b90909db6d6f6d4e
kylejb <kjburda@gmail.com>
2020-08-19 21:33:16 -0400

    added 'credentials: true'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # root 'static_pages#home'
+  
+  
   resources :classrooms, only: [:index, :show]
 
   post '/meetings' => 'zoom_meetings#create', as: 'meetings'
